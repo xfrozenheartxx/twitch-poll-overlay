b4411707b13a0d19fbb2342d00946e96db44a218
frutzi <78870673+xfrozenheartxx@users.noreply.github.com>
2022-12-05 17:27:44 -0600

    Update bar.ts

--- src/ts/charts/bar.ts
+++ src/ts/charts/bar.ts
@@ -160,7 +160,7 @@ export class Bar extends Chart {
       if (drawText) {
         let txt: string;
         if (prop.textRound) {
-          txt = `${Math.round(percentage * 340)}%`;
+          //txt = `${Math.round(percentage * 340)}%`;
         } else {
           txt = `${percentage * 360}%`;
         }
